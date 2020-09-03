83d0fc56105c0fa77886af02a36c22ea2ee242f8
Danilo Bizarria <danilobizarria@gmail.com>
2020-09-03 00:04:31 -0300

    add function definition, call and return to AST

--- src/com/craftinginterpreters/lox/Stmt.java
+++ src/com/craftinginterpreters/lox/Stmt.java
@@ -5,10 +5,11 @@ import java.util.List;
 abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
-    R visitExitStmt(Exit stmt);
     R visitExpressionStmt(Expression stmt);
+    R visitFunctionStmt(Function stmt);
     R visitIfStmt(If stmt);
     R visitPrintStmt(Print stmt);
+    R visitReturnStmt(Return stmt);
     R visitVarStmt(Var stmt);
     R visitWhileStmt(While stmt);
   }
@@ -24,20 +25,6 @@ abstract class Stmt {
 
     final List<Stmt> statements;
   }
-  static class Exit extends Stmt {
-    Exit(Token operator, Expr value) {
-      this.operator = operator;
-      this.value = value;
-    }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-      return visitor.visitExitStmt(this);
-    }
-
-    final Token operator;
-    final Expr value;
-  }
   static class Expression extends Stmt {
     Expression(Expr expression) {
       this.expression = expression;
@@ -49,6 +36,22 @@ abstract class Stmt {
     }
 
     final Expr expression;
+  }
+  static class Function extends Stmt {
+    Function(Token name, List<Token> params, List<Stmt> body) {
+      this.name = name;
+      this.params = params;
+      this.body = body;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitFunctionStmt(this);
+    }
+
+    final Token name;
+    final List<Token> params;
+    final List<Stmt> body;
   }
   static class If extends Stmt {
     If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
@@ -77,6 +80,20 @@ abstract class Stmt {
     }
 
     final Expr expression;
+  }
+  static class Return extends Stmt {
+    Return(Token keyword, Expr value) {
+      this.keyword = keyword;
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitReturnStmt(this);
+    }
+
+    final Token keyword;
+    final Expr value;
   }
   static class Var extends Stmt {
     Var(Token name, Expr initializer) {
