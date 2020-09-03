b05929c9e3f96124f8c129858ca6314dfa4c179f
Danilo Bizarria <danilobizarria@gmail.com>
2020-09-03 00:09:34 -0300

    add function definition, call and return to Interpreter
    also implement native (called directly from Java) clock()

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,13 +1,34 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter
     implements Expr.Visitor<Object>,
         Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
+    final Environment globals = new Environment();
+    private Environment environment = globals;
 
     private final Object unassigned = new Object();
+
+    Interpreter() {
+        // other native functions:
+        // reading input from the user,
+        // working with files, etc.
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() { return 0; }
+
+            @Override
+            public Object call(Interpreter interpreter,
+                               List<Object> arguments) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() { return "<native fn>"; }
+        });
+    }
 
     // before inclusion of statements
 //    void interpret(Expr expression) {
@@ -29,7 +50,7 @@ class Interpreter
         stmt.accept(this);
     }
 
-    private void executeBlock(List<Stmt> statements, Environment environment) {
+    void executeBlock(List<Stmt> statements, Environment environment) {
         Environment previous = this.environment;
         try {
             this.environment = environment;
@@ -51,8 +72,11 @@ class Interpreter
     }
 
     @Override
-    public Void visitExitStmt(Stmt.Exit stmt) {
-        throw new RuntimeError(stmt.operator, "Loop exit issued out of loop construct.");
+    public Void visitReturnStmt(Stmt.Return stmt) {
+        Object value = null;
+        if(stmt.value != null) value = evaluate(stmt.value);
+
+        throw new Return(stmt.keyword, value);
     }
 
     @Override
@@ -64,6 +88,13 @@ class Interpreter
 //            String text = stringify(exprVal);
 //            if(exprVal instanceof String) text = "'" + text + "'";
 //            System.out.println(text);
+        return null;
+    }
+
+    @Override
+    public Void visitFunctionStmt(Stmt.Function stmt) {
+        LoxFunction function = new LoxFunction(stmt, environment);
+        environment.define(stmt.name.lexeme, function);
         return null;
     }
 
@@ -101,10 +132,9 @@ class Interpreter
         while(isTruthy(evaluate(stmt.condition))) {
             try {
                 execute(stmt.body);
-            } catch(RuntimeError error) {
-                if(error.token.type == TokenType.BREAK) break;
-                if(error.token.type == TokenType.CONTINUE) continue;
-                throw error;
+            } catch(Return returnStmt) {
+                if(returnStmt.keyword.type == TokenType.BREAK) break;
+                // continue unnecessary, as loop will go on from here.
             }
         }
         return null;
@@ -170,6 +200,31 @@ class Interpreter
 
         // Unreachable
         return null;
+    }
+
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for(Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        if(!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren,
+        "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable)callee;
+
+        if(arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected " +
+                function.arity() + " arguments, but got " +
+                    arguments.size() + ".");
+        }
+
+        return function.call(this, arguments);
     }
 
     @Override
