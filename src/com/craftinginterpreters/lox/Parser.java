81fc18b933124a44215bfe6141fa82ec88d7618e
Danilo Bizarria <danilobizarria@gmail.com>
2020-12-02 16:46:24 -0300

    add parsing of Get, Set, This Exprs and Class Stmt

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -33,6 +33,7 @@ class Parser {
 
     private Stmt declaration() {
         try {
+            if(match(CLASS)) return classDeclaration();
             if(match(FUN)) {
                 Stmt stmt = new Stmt.Expression(function("function"));
                 // handle fun() {};
@@ -48,8 +49,22 @@ class Parser {
         }
     }
 
+    private Stmt classDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+
+        List<Expr.Function> methods = new ArrayList<>();
+        while(!check(RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+
+        return new Stmt.Class(name, methods);
+    }
+
 //    private Stmt function(String kind) {
-    private Expr function(String kind) {
+    private Expr.Function function(String kind) {
         Token name = null;
         if(check(IDENTIFIER)) {
             name = advance();
@@ -233,6 +248,9 @@ class Parser {
             if(expr instanceof Expr.Variable) {
                 Token name = ((Expr.Variable)expr).name;
                 return new Expr.Assign(name, value);
+            } else if (expr instanceof Expr.Get) {
+                Expr.Get get = (Expr.Get)expr;
+                return new Expr.Set(get.object, get.name, value);
             }
 
             error(equals, "Invalid assignment target.");
@@ -345,6 +363,10 @@ class Parser {
         while(true) {
             if(match(LEFT_PAREN)) {
                 expr = finishCall(expr);
+            } else if (match(DOT)) {
+                Token name = consume(IDENTIFIER,
+                    "Expect property name after '.'.");
+                expr = new Expr.Get(expr, name);
             } else {
                 break;
             }
@@ -377,6 +399,8 @@ class Parser {
         if(match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
         }
+
+        if(match(THIS)) return new Expr.This(previous());
 
         if(match(IDENTIFIER)) {
             return new Expr.Variable(previous());
